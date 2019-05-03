e59125294eccfa8b5c98331253b1714d0a400851
PDmatrix <dimasebakovgoo@gmail.com>
2019-05-03 16:52:07 +0300

    Favicon

--- src/UI/src/components/Post/Post.tsx
+++ src/UI/src/components/Post/Post.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PostDto } from '@/api';
-import { Divider, Tag, Tooltip } from 'antd';
+import { Divider, Tag, Tooltip, Typography } from 'antd';
 import { Segment } from '@/components/Shared/Segment';
 import moment from 'moment';
 
@@ -11,18 +11,20 @@ interface IPost {
 const Post: React.FC<IPost> = ({ post }) => {
   return (
     <Segment>
-      <h3>{post.title}</h3>
-      <Divider />
-      <p>{post.content}</p>
-      Теги: <Tag> {post.tag}</Tag>
-      <Divider />
-      <p>
-        <Tooltip title={moment(post.date).format('YYYY-MM-DD HH:mm:ss')}>
-          <span>Опубликовано {moment(post.date).fromNow()}</span>
-        </Tooltip>
-        <br />
-        пользователем {post.username}
-      </p>
+      <Typography>
+        <Typography.Title level={4}>{post.title}</Typography.Title>
+        <Divider />
+        <Typography.Paragraph>{post.content}</Typography.Paragraph>
+        Теги: <Tag> {post.tag}</Tag>
+        <Divider />
+        <Typography.Paragraph>
+          <Tooltip title={moment(post.date).format('YYYY-MM-DD HH:mm:ss')}>
+            <span>Опубликовано {moment(post.date).fromNow()}</span>
+          </Tooltip>
+          <br />
+          пользователем {post.username}
+        </Typography.Paragraph>
+      </Typography>
     </Segment>
   );
 };
